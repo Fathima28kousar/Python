9a2cebd19ea75725b82d3da3a61608d84b4b6864
Fathima28kousar <kousar28fathima@gmail.com>
2023-07-27 19:49:10 +0530

    learn py

---
+++ myfirstclass.py
@@ -0,0 +1,26 @@
+print("Hello")
+import keyword
+print (keyword.kwlist)
+print(len(keyword.kwlist))
+name="fathima"
+age=12
+name="lubna"
+age="10"
+print(name)
+print(age)
+is_adult = True
+
+name = "Tony Stark"
+age = "51"
+is_genius = True
+print(name)
+print(age)
+
+number = 18
+print(float(number))
+first = input("enter first number : ")
+second = input("enter second number : ")
+sum = int(first) + int(second)
+print(sum)
+
+
