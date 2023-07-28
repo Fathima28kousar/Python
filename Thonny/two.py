2adcee2ac10629e3c8d163ad2abb42b93fabbedf
Fathima28kousar <kousar28fathima@gmail.com>
2023-07-28 20:43:27 +0530

    Practice

---
+++ Thonny/two.py
@@ -0,0 +1,120 @@
+'''
+first_number = 100
+second_number =200
+sum = int(first_number)+int(second_number)
+print("the sum is : " + str(sum))
+'''
+'''name = "Tony Stark"
+print(name.upper())
+print(name.lower())
+print(name)'''
+'''name = "Tony Stark"
+print(name.find('S'))
+print(name.find('s'))
+print(name.find('Stark'))
+print(name.replace("T","M"))
+print(name)'''
+age = 100
+'''if age >= 18:
+    print("you are an adult")
+    print("you can vote")
+elif age <18 and age >5:
+    print("you are in school")
+    print("you cannot vote")
+else:
+    print("you are a kid")
+    print("you cannot vote")
+    
+print("Thank you")    
+
+numbers = range(9)
+print(numbers)'''
+print("Hello")
+name="fathima"
+age=10
+is_adult=False
+print(name)
+print(float(age))
+print(is_adult)
+'''name = input("What is your name ? ")
+print("Hello " + name)'''
+a=10
+b=12
+print(a+b)
+'''old_age = input("Enter your old age : ")
+new_age = int(old_age) + 2
+print(new_age)'''
+'''first = input("Enter the first number : ")
+second = input("Enter the second number : ")
+print(int(first) + int(second))'''
+'''first_name = input("Enter your first name : ")
+last_name = input("Enter your last name : ")
+age = 58
+is_genius = True
+print(first_name + " " + last_name)
+print("Hello " + first_name + " " + last_name)
+print(age)
+print(is_genius)'''
+first_number = 100
+second_number = 200
+sum = int(first_number) + int(second_number)
+print("the sum is : " + str(sum))
+name = ("Hello worlD !")
+print(name.lower())
+name = "Burj KHalifa"
+print(name.find('lifa'))
+name = "atif aslam"
+print(name.replace("a","k"))
+print(name)
+name = "Dubai"
+print("k" in name)
+print(5**2)
+i=5
+i+=5
+print(i)
+#Hey my name is fathima what is your name 
+print(not 3>2 and 3==2)
+age = 2
+if age >=18:
+    print("You are an adult")
+    print("You can vote")
+elif age <18 and age >3:
+    print("You are a child")
+    print("Sorry You cannot vote")
+else:
+   
+    print("you are a kid")
+    print("Sorry You cannot vote")
+numbers = range(200)
+print(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
