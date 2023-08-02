2ab783389aaa2c830e1342da8191c91655009b0b
Fathima28kousar <kousar28fathima@gmail.com>
2023-08-02 14:21:51 +0530

    identifiers

---
+++ Thonny/hello.py
@@ -0,0 +1,40 @@
+'''l1 = []
+l3 = [10, 20, 30, 40]
+l4 = [100, 200, 100, 100]
+l5 = [10, 2.5, "Rahul", True, [], {}, ()]
+print(l1)
+print(l3)
+print(l4)
+print(l5)'''
+'''a@b = 10
+print(a@b)'''
+'''a = int(input("Enter your number:"))
+print(a)'''
+'''i=1
+while i<= 5:
+    print(i)
+    i=i+1'''
+'''for item in range(5):
+    print(item)'''
+    
+marks = [97,89,99]
+i = 0
+while i<len(marks):
+    print(marks[i])
+    i=i+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Thonny/welcome.py
+++ Thonny/welcome.py
@@ -1,10 +1,100 @@
-line01 = "*****************"
-line02 = "*               *"
-line03 = "*    WELCOME    *"
+'''price = 341
+rating = 3.6
+size1 = 'M'
+size2 = "M"
+avail = True
+sizes = [38,40,42,44]
+sizes1 = (10,20,30,40)
+sizes2 = {38,40,42,44}
+print(price)
+print(rating)
+print(size1)
+print(size2)
+print(avail)
+print(sizes)
+print(sizes1)
+print(sizes2)'''
 
-print('')
-print(line01)
-print(line02)
-print(line03)
-print(line02)
-print(line01)
+'''print("***Create operations***")
+price = 341
+rating = 3.6
+ename = "Rahul"
+c = 10 + 20J
+avail = True
+l = [10,20,30,40]
+b = bytes(l)
+ba = bytearray(l)
+size = [38,40,42,44]
+s = {38,40,42,44}
+t = (99, 98 , 97)
+dict = {'id': 101}
+fa = frozenset()
+r = range(18)
+print(range)
+print(id(range))
+print(type(range))'''
+s = {10}
+'''fa = frozenset(s)
+print(type(fa))
+
+r = range(10)
+print(r)
+print(type(r))
+
+a = 10
+a = 20
+print(id(a))'''
+
+'''print(15)
+print(bin(15))
+print(oct(15))
+print(hex(15))
+
+a = 15
+b = 0b1111
+c = 0o17
+d = 0xf
+
+
+print(a)
+print(b)
+print(c)
+print(d'''
+'''sal = 45000.65
+print(sal)
+price = 499
+print(float(price))
+print(float(20))
+print(float(False))
+
+c1 = 10 + 5j
+c2 = 10 + 5J
+c3 = 10.5 + 5j
+c4 = 10 + 5.5j
+c5 = 10.5 + 5.5j
+print(c1)
+print(c2)
+print(c3)
+print(c4)
+print(c5)'''
+l = [10, 20, 30, 40, 50]
+b = bytes(l)
+print(b[0])
+b[0] = 90
+print(b[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
