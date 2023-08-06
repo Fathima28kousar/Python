d487ae2b3d33458f76a8120e36de9c167227ac78
Fathima28kousar <kousar28fathima@gmail.com>
2023-08-06 18:23:43 +0530

    git

---
+++ Thonny/boolean_data.py
@@ -0,0 +1,68 @@
+first = "Fathima"
+print(first.startswith("D"))
+print(first.endswith("a"))
+
+myvalue = "String"
+print(isinstance(myvalue, bool))
+x = bool(False)
+print(type(x))
+
+price = 100
+best_price = int(80)
+print(type(price))
+print(isinstance(best_price, int))
+
+gpa = 4.2809
+print(type(gpa))
+y = float(200)
+print(isinstance(y, float))
+
+comp_value = 5 + 3j
+print(type(comp_value))
+print(comp_value.real)
+print(comp_value.imag)
+print()
+print(abs(gpa))
+print(abs(gpa * -1))
+print(round(gpa))
+print(round(gpa, 1))
+print()
+import math
+print(math.pi)
+print(math.sqrt(64))
+print(math.ceil(gpa))
+print(math.floor(gpa))
+print()
+zipcode = "10001"
+zip_value = int(zipcode)
+print(type(zip_value))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Thonny/coffee_menu.py
+++ Thonny/coffee_menu.py
@@ -4,3 +4,24 @@ print(title.center(20, "+"))
 print("Coffee".ljust(16, ".") +"$1".rjust(4))
 print("Muffin" .ljust(16, ".") +"$2".rjust(4))
 print("cheese cake".ljust(16, ".") +"$3".rjust(4))
+print()
+title = "menu".upper()
+print(title.center(20, "+"))
+print("Coffee" .ljust(16, ".") + "$1".rjust(4))
+print("Tea" .ljust(16, ".") + "$2".rjust(4))
+print("cherry" .ljust(16, ".") + "$4".rjust(4))
+
+print("")
+first = "Fathima Kousar is a software developer"
+print(first[18])
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
