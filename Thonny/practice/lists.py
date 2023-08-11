78eb13f88b5f6e7d6ebf54e0a297b9bc9d4d9d9b
Fathima28kousar <kousar28fathima@gmail.com>
2023-08-11 17:40:26 +0530

    git

---
+++ Thonny/practice/lists.py
@@ -0,0 +1,64 @@
+L =[1,5,9]
+print(sum(L)+max(L)-min(L))
+
+L = [1,2,3,4,5,6,7,8,9,10]
+# print(index[0])
+print(L[len(L)-1])
+print(1 in [[1],2,3])
+
+# print(len(tuple[1]))
+# D = list[]
+# print(len[D])
+l = [4,5,5,5,5,6,7]
+l.remove(5)
+print(l)
+import copy
+list1 = [1,2,3,4]
+list2 = copy.copy(list1)
+print(list2)
+
+
+def listchange(L):
+    L.append(45)
+    return
+L1 = [1,2,3,4]
+listchange(L1)
+print(L1)
+print([]*10)
+
+L = [1,21,4,2,5,9,6,7]
+print(L[1:8:2])
+print(L[1::2])
+
+L = ["Amit" ,"anita" ,"Sumanth" ,"Zaid"]
+print(max(L))
+L = [13,12,15,27,3,46]
+list1.pop(3)
+print(L)
+list1 = [3,2,5,3,6,4]
+list1.insert(10,3)
+print(list1)
+L = ["amit" ,"sumit","ravi"]
+print("@".join(L))
+
+L1 = [1,2,3,4,5]
+L2 = [1,2,3]
+print([L2] in L1)
+A = []
+A
+print(A)
+l1= "hello"
+print(type(l1))
+l2 = list(l1)
+print(l2)
+
+
+
+
+
+
+
+
+
+
+
--- Thonny/practice/pratice.py
+++ Thonny/practice/pratice.py
@@ -31,3 +31,93 @@ ids.append(104)
 print(ids)
 ids.insert(0,105)
 print(ids)
+enames = ["rahul","soniya"]
+enames.append("priyanka")
+print(enames)
+l1 = [10,20,30,40,50]
+l2 = [60,70]
+l1.extend(l2)
+# print(l1)
+# l1.extend([100])
+# print(l1)
+# enames.remove("soniya")
+# print(enames)
+# #enames.remove(10)
+# print(l1)
+# print(enames)
+# el = enames.pop()
+# print(enames)
+# print(el)
+l1 = [10,20,30,40]
+l2=l1.copy()
+print(l1)
+l2[2] = 45
+print(l2)
+print(l1)
+l1 = l2 
+print(l1)
+l1  = [400,300,200,500]
+l2 = l1 [:]
+print(l2)
+####tuples
+t1 = (40,20,10,30)
+# t1.reverse()
+# print(t1)
+l1.sort()
+print(l1)
+print(t1)
+l1 = sorted(t1)
+print(type(l1))
+t1 = tuple(l1)
+print(t1)
+l1 = [1,2,3]
+l2 = [1,2,3,4,5,6]
+l = ["amit", "anita" , "zee", "longestword"]
+print(max(l))
+l1 = list("www.csiplearninghub.com")
+print(l1[20:1])
+l2 = ["amit","sumit","naina"]
+print(l2[-1][-1])
+# print(l2**3)
+l= [0.5*x for x in range(4)]
+print(l)
+li = [1*x for x in range(10,1,-4)] 
+print(li)
+
+# for i in L:
+#     print(i,end=" ")
+#     i = i+1
+# L = ["amit","sumit","naina"] 
+# L1 = ["sunil"]
+# # print(L+L1)  
+# L.append("4")
+# l = 
+# print(L)
+L = [1,2,3,4,5]
+L.append(6)
+print(L)
+
+L = "123456"
+L = list(L)
+print(type(L[0]))
+
+T = (1,2,3,4,5.5)
+L = list(T)
+print(L[3]*2.5)
+
+T = [1,2,3,4]
+T1=T
+T[0] ="A"
+print(T)
+print(T1)
+
+T = [1,2,3,4]
+T1 =[5,6,7]
+T.append(T1)
+print(T)
+
+L = ["amit" ,"sumit" ,"naina"]
+L1 = ["sumit"]
+print(L+L1)
+L =[1,5,9]
+print(sum(L)+max(L)-min(L))
