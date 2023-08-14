dad178a283bf6de915c80f5cb4af01e5c9003f5e
Fathima28kousar <kousar28fathima@gmail.com>
2023-08-14 15:23:11 +0530

    python

---
+++ Thonny/practice2/harry.py
@@ -0,0 +1,77 @@
+# r= round(34/5)
+# print(r)
+# x = True
+# y = False
+# z = True
+# print(not x)
+# print(x and y)
+# print(x or y)
+# print(y and x)
+# print(x and z)
+# print(y and z)
+
+# first = "Dave"
+# second = "Gray"
+# # print(first)
+# # print(type(first))
+# # print(type(first)==str)
+# # print(isinstance(first,str))
+
+# # fullname = first + "   " + second
+# # print(fullname)
+# # fullname += "  !!!!"
+# # print(fullname)
+# # decade = str(1989)
+# # print(decade)
+# # print(type(decade))
+# # statement  = " i like musid of " + decade + "s."
+# # print(statement)
+# multiline = '''          
+#            hello                '''
+# print(multiline)
+# # sentence = "I\'m back to home\nI\'m good\t\thello"
+# # print(sentence)
+# # first = "fathima fatHima fathIma"
+# # print(first.lower())
+# # print(first.upper())
+# # print(first.replace("fathima","kousar"))
+# # print(first.title())
+# # print(first.capitalize())
+# print(len(multiline))
+# print(multiline.strip())
+# print(len(multiline.strip()))
+# print(multiline.rstrip())
+# print(len(multiline.rstrip()))
+# print(multiline.lstrip())
+# print(len(multiline.lstrip()))
+
+# first = "fathima"
+# # print(first[0])
+# # print(first[:])
+# print(first.startswith("f"))
+# print(first.endswith("a"))
+# myvalue = True
+# x = bool(False)
+# print(type(x))
+# print(isinstance(myvalue,bool))
+# price = 100
+# print(type(price))
+# print(isinstance(price,int))
+# comp_value = 5+3j
+# print(type(comp_value))
+# print(comp_value.real)
+# print(comp_value.imag)
+# gpa = 3.28
+# print(gpa)
+# print(abs(gpa))
+# print(round(gpa))
+# print(round(gpa,1))
+# a = "10001"
+# b = int(a)
+# print(b)
+# print(type(b))
+# a = "newyork"
+# b = int(a)
+# print(b)
+value = int(input("enter your name:\n "))
+print(value)
