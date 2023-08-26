6ab18ebbb9fc3f359bd1177013686ed6653eff7c
Fathima28kousar <kousar28fathima@gmail.com>
2023-08-26 23:12:58 +0530

    L

---
+++ py_native_practice/loops.py
@@ -0,0 +1,16 @@
+# n = int(input("Enter the num : "))
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print(j+1,end=" ")
+#     print()         
+
+# i = 0
+# while i<=7:
+#     print(i*7)
+#     i = i+1
+
+n = int(input("enter the number : "))
+for i in range(n ):
+    for j in range(i+1):
+        print(j+1,end = " ")
+    print()
