dbfed8eda8c2e91d36874d8100213131e43dcdd0
Fathima28kousar <kousar28fathima@gmail.com>
2023-08-28 15:31:28 +0530

    O

---
+++ Thonny/practice2/module.py
@@ -0,0 +1,77 @@
+# import math
+# print(math.floor(9.8))
+
+# import math
+# print(math.floor(-40.36))
+ 
+# import math
+# print(math.ceil(-40.36))
+
+# import math
+# print(math.ceil(9.8))
+
+# import math
+# print(math.pow(2,3))
+
+# import math
+# print(math.sqrt(16))
+
+
+
+# import math
+# print(math.fabs(-9.8))
+
+# import math
+# print(math.fabs(10))
+
+# import math
+# print(math.sin(0))
+
+# import math 
+# print(math.sin(90))
+
+# import math
+# print(math.pi)
+# import math
+# print(math.e)
+
+# import statistics
+# print(statistics.median([23,45,56,76,78]))
+
+# import statistics
+# print(statistics.mean([1,2,3,4,5,6,7,8,9,10]))
+
+# import statistics
+# print(statistics.mode([1,2,1,4,1,2,3,2,1,5]))
+# import random
+# a = random.randrange(20)
+# print(a)
+
+# import random 
+# a = random.randrange(1)
+# print(a)
+
+
+# import random 
+# name = ["Amit", "Sumit", "Naina" , "Suman", "Parth"]
+# rd = random.randrange(len(name))
+# print(name[rd])
+
+# import random 
+# name = ["Amit", "Sumit", "Naina" , "Suman", "Parth"]
+# rd = random.randrange(5)
+# print(name[rd])
+# import random
+
+# highest = int(input("Enter the highest number: "))
+# lowest = int(input("Enter the lowest number: "))
+# even_numbers = []
+
+# while len(even_numbers) < 5:
+#     num = random.randint(lowest, highest)
+#     if num % 2 == 0:
+#         even_numbers.append(num)
+
+# print("Random even numbers:", even_numbers)
+import math
+print(math.floor(23.99))  #o/u 9
