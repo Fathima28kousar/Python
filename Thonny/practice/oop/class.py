34d6f09e1dfcbc3694186939084406748568f711
Fathima28kousar <kousar28fathima@gmail.com>
2023-09-07 19:23:47 +0530

    A

--- Thonny/practice/oop/class.py
+++ Thonny/practice/oop/class.py
@@ -1,55 +1,112 @@
-class Employee:
-    "hello world"
-    company_name = "TCS"
-    def __init__(self,sal,age):
-        self.sal = sal
-        self.age = age
+# class Employee:
+#     "hello world"
+#     company_name = "TCS"
+#     def __init__(self,sal,age):
+#         self.sal = sal
+#         self.age = age
 
-    def display(self):
-        print(self.sal,self.age)
+#     def display(self):
+#         print(self.sal,self.age)
 
-    def change_data(self):
-        self.age = 35
-        self.sal = 100
-    @classmethod
-    def get_company_name(cls):
-        cls.company_name = "Royal"
-        print(cls.company_name)
+#     def change_data(self):
+#         self.age = 35
+#         self.sal = 100
+#     @classmethod
+#     def get_company_name(cls):
+#         cls.company_name = "Royal"
+#         print(cls.company_name)
     
-e1 = Employee(1000,20)
-e2 = Employee(2000,40)
-Employee.get_company_name()
+# e1 = Employee(1000,20)
+# e2 = Employee(2000,40)
+# Employee.get_company_name()
 
 
+'''class Employee:
+    def __init__(self,first,last,pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first +"." +last+"@company.com"
+    
+    def fullname(self):
+        return "{}{}".format(self.first,self.last)
+    
+e1 = Employee("atif ","aslam ",700)
+e2 = Employee("fathima","kousar",1000)
+'''
+
+'''class Employee:
+    raise_amount = 1.04
+    num_of_emps = 0
+
+    def __init__(self,first,last,pay):
+        self.first = first 
+        self.last = last
+        self.pay = pay
+        Employee.num_of_emps+=1
+
+    def apply_raise(self):
+        self.pay = int(self.pay*self.raise_amount)  
+
+    @classmethod
+    def set_raise_amt(cls,amount):
+        cls.raise_amt = amount
+        Employee.set_raise_amt(1.05)
 
 
+e1 = Employee("tina","malhotra",200)
+e2 = Employee("rahul","khanna",100)
+
+e1.raise_amount = 1.05
+print(e1.__dict__)
+print(Employee.raise_amount)
+print(e1.raise_amount)
+print(Employee.num_of_emps)
+print(Employee.__dict__)
+
+'''
+
+# class Employee:
+#     bonus = 1.04
+#     num_of_emps = 0
+
+#     def __init__(self,first,last,pay):
+#         self.first = first
+#         self.last = last
+#         self.pay = pay
+#         Employee.num_of_emps+=1
+
+#     def fullname(self):
+#         return "{}{}".format(self.first,self.last)
+    
+#     def apply_raise(self):
+#         self.pay = int(self.pay*self.bonus)
 
 
+# e1 = Employee("fathima","kousar",40)
+# e2 = Employee("Lubna ","sadiay",50)
 
-
-# print(Employee.company_name)
-# print(e1.company_name)
-# print(e2.company_name)
-# Employee.company_name = "Infosys"
-# print(Employee.company_name)
-# e1.company_name = "Licious"
-# print(e1.company_name)
-# print(Employee.company_name)
+# e1.bonus = 1.08
 # print(e1.__dict__)
-# print(Employee.__dict__)
-# # e1.change_data()
-# e2.change_data()
-# print(e2.__dict__)
-# # e1.name = "Rahul"
-# print(e1.__dict__)
-# del e1.name
-# print(e1.__dict__)
-# print(e1.sal)
-# e1.sal = 5000
-# print(e1.sal)
-# e2.display()
-# print(Employee.__dict__)
-# print(Employee.__doc__)
-# print(Employee.__name__)
-# print(Employee.__module__)
-# print(isinstance(e1,Employee))
+# print(Employee.bonus)
+# print(Employee.apply_raise  )
+# print(e1.bonus)
+# print(e1.bonus)
+
+
+'''class Employee:
+    company_name = "TCS"
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+
+e1 = Employee("rahul",123)
+e2 = Employee("soniya",3444)
+
+Employee.company_name = "Fortis"
+print(Employee.company_name)
+
+e2.company_name = "capegemini"
+print(e2.company_name)
+print(Employee.company_name)
+'''
--- py_native_practice/oop.py
+++ py_native_practice/oop.py
@@ -1,8 +1,24 @@
 #OOP Exercise 1: Create a Class with instance attributes,,,Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.***********
-class Vehicle:
+'''class Vehicle:
     def __init__(self,max_speed,milaege):
         self.max_speed = max_speed
         self.milaege = milaege
 
 model = Vehicle(240,70)
-print(model.max_speed,model.milaege)       
+print(model.max_speed,model.milaege)       '''
+#Create a Vehicle class without any variables and methods**************
+'''class Vehicle:
+    pass'''
+
+class Vehicle:
+    def __init__(self,max_speed,milaege):
+        self.max_speed = max_speed
+        self.milaege = milaege
+
+
+class Bus(Vehicle):
+    def vehicle_name(self):
+        print("vehicle is good")
+
+b1 = Bus()
+print(b1.vehicle_name(240kmph,29))
