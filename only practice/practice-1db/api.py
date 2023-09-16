3f9149209a2cdae77b3cc88f6f019568d320ff26
Fathima28kousar <kousar28fathima@gmail.com>
2023-09-16 19:56:03 +0530

    D

--- only practice/practice-1db/api.py
+++ only practice/practice-1db/api.py
@@ -85,10 +85,131 @@ Requied filed: None
 
 #invoke rest api and write into new json file
 #invoke rest api and write into new json file
+# '''import random 
+
+# name = input("What is your name? ")
+# print("Good Luck ",name,"!")
+
+# words = ["apple","hello","hi"]
+# word = random.choice(words)
+
+# guesses = " "
+
+# turns=10
+# while turns>0:
+#     failed=0
+
+#     for char in word:
+#         if char in guesses:
+#             print(char,end="")
+
+#         else:
+#             print("_",end=" ")
+#             failed+=1
+        
+#     if failed ==0:
+#         print("\n\'you win\' ")
+#         print("The word is ",word)
+#         break
+
+#     print()
+
+#     guess = input("Enter the character: ")
+#     guesses +=guess
+#     guess = guess.lower()
+
+#     if guess not in word:
+#         turns-=1
+#         print("Wrong")
+#         print("You have ",turns,"more turns")
+
+#         if turns ==0:
+#             print("You loose!")
+#             print("The correct word is: ",word)'''
+
+import random
+from collections import Counter
+
+someWords = '''
+apple banana mango strawberry
+orange grape pineapple apricot lemon coconut watermelon
+cherry papaya berry peach lychee muskmelon
+'''
+
+someWords = someWords.split()
+word = random.choice(someWords)
+
+if __name__ == '__main__':
+    print('Guess the word! HINT: The word is a name of a fruit')
+
+    for i in word:
+        # For printing the empty spaces for letters of the word
+        print('_', end=' ')
+    print()
+
+    playing = True
+    # List for storing the letters guessed by the player
+    letterGuessed = ''
+    chances = len(word) + 2
+    correct = 0
+    flag = 0
+
+    try:
+        while chances != 0 and flag == 0:  # Flag is updated when the word is correctly guessed
+            print()
+            chances -= 1
+
+            try:
+                guess = str(input('Enter a letter to guess: '))
+            except:
+                print('Enter only a letter!')
+                continue
+
+            # Validation of the guess
+            if not guess.isalpha():
+                print('Enter only a LETTER')
+                continue
+            elif len(guess) > 1:
+                print('Enter only a SINGLE letter')
+                continue
+            elif guess in letterGuessed:
+                print('You have already guessed that letter')
+                continue
+
+            # If letter is guessed correctly
+            if guess in word:
+                # k stores the number of times the guessed letter occurs in the word
+                k = word.count(guess)
+                for _ in range(k):
+                    letterGuessed += guess  # The guessed letter is added as many times as it occurs
+
+            # Print the word
+            for char in word:
+                if char in letterGuessed and (Counter(letterGuessed) != Counter(word)):
+                    print(char, end=' ')
+                    correct += 1
+                # If user has guessed all the letters
+                # Once the correct word is guessed fully,
+                elif Counter(letterGuessed) == Counter(word):
+                    # The game ends, even if chances remain
+                    print("The word is:", end=' ')
+                    print(word)
+                    flag = 1
+                    print('Congratulations, You won!')
+                    break  # To break out of the for loop
+                    break  # To break out of the while loop
+                else:
+                    print('_', end=' ')
+
+        # If user has used all of their chances
+        if chances <= 0 and (Counter(letterGuessed) != Counter(word)):
+            print()
+            print('You lost! Try again..')
+            print('The word was {}'.format(word))
+
+    except KeyboardInterrupt:
+        print()
+        print('Bye! Try again.')
 
 
-    
-    
-            
-
- 
+        
--- only practice/practice-1db/one.py
+++ only practice/practice-1db/one.py
@@ -1,50 +1,7 @@
-import random
+# print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star,\n\t How I wonder what you are")
 
-fruits = [
-    "apple", "banana", "mango", "pineapple", "strawberry",
-    "watermelon", "papaya", "grapes", "orange", "lemon",
-    "coconut", "muskmelon", "peach", "apricot"
-]
-
-word = random.choice(fruits)
-
-if __name__ == '__main__':
-    print("Guess the word! Hint: The word is a name of a fruit")
-
-    guessed_letters = ""
-    chances = len(word) + 2
-    flag = False
-
-    while chances > 0 and not flag:
-        print("\nChances are:", chances)
-        print("You guessed-",guessed_letters)
-        chances -= 1
-
-        guess = input("Enter a letter to guess: ").lower()
-
-        if not guess.isalpha() or len(guess) != 1:
-            print('Please enter a single letter.')
-            continue
-
-        if guess in guessed_letters:
-            print("You have already guessed that letter.")
-            continue
-
-        guessed_letters += guess
-
-        if guess in word:
-            for char in word:
-                if char in guessed_letters:
-                    print(char, end=" ")
-                else:
-                    print("_", end=" ")
-            if set(word) == set(guessed_letters):
-                print("\nThe word is:", word)
-                flag = True
-                print("Congratulations! You won!")
-        else:
-            print("Incorrect guess. Try again.")
-
-    if not flag:
-        print('\nYou ran out of chances.')
-        print('The word was:', word)
+import sys
+print("python version")
+print(sys.version)
+print("version info.")
+print(sys.version_info)
