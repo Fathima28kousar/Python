2efb6aced83ba7ff04586b8550e86f8d6328e654
Fathima28kousar <kousar28fathima@gmail.com>
2023-09-19 23:05:54 +0530

    D

--- only practice/practice-1db/.py
+++ only practice/practice-1db/.py
@@ -63,32 +63,78 @@ db.commit()
 dbcursor.close()
 db.close()
 '''
+# import mysql.connector
+# try:
+#     conn = mysql.connector.connect(
+#         host= "localhost",
+#         user="root",
+#         password = "roots",
+#         database="codeyug1"
+#     )
+
+#     if conn.is_connected():
+#         print("Connected!")
+
+# except Exception as obj:
+#     print("Not connected")
+
+# cur = conn.cursor()
+# cur.execute("""CREATE TABLE IF NOT EXISTS tutorial1(
+#             id INT PRIMARY KEY,
+#             name VARCHAR(50),
+#             views INT,
+#             watchtime FLOAT
+#             )""")
+# cur.execute("DESC tutorial1")
+# for data in cur:
+#     print(data[0])    
+# sql = """
+#     INSERT INTO tutorial1(id,name,views,watchtime)VALUES(101,"OOP basics",1500,10)
+# """
+# cur.execute(sql)
+
+# conn.commit()
+
+# cur.close()
+# conn.close()
 import mysql.connector
 try:
-    conn = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password="roots",
-        database="codeyug"
+    db = mysql.connector.connect(
+        host= "localhost",
+        user="root",
+        password = "roots",
+        database="codeyug1"
     )
 
-    if conn.is_connected():
-        print("connected")
-except Exception as obj:
-    print(obj)
-cur = conn.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS tutorial(
-    video_id INT PRIMARY KEY,
-    video_name VARCHAR(100) NOT NULL,
-    video_views INT,
-    watchtime FLOAT
-)""")  
-cur.execute("DESC tutorial")
-for data in cur:
-    print(data[0])  
-conn.commit()
-cur.close()
-conn.close()
+    if db.is_connected():
+        print("Connected!")
 
+except Exception as obj:
+    print("Not connected")
+
+cur = db.cursor()
+
+sql = "SELECT * FROM tutorial1"
+
+
+
+try:
+    cur.execute(sql)
+    data = cur.fetchall()
+    for ele in data:
+        print(ele)
+    for ele in data:
+        print(ele)
+    print(list(data))   
+    print(f"{len(data)} rows got fetched") 
+    db.commit()
+    print()
+
+except:
+    db.rollback()
+    print("Something went wrong")
+
+cur.close()
+db.close()
 
 
