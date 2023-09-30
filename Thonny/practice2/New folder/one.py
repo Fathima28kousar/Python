0e4751b2f4d2860f2836319a1ac29fbf0dfdc8f3
Fathima28kousar <kousar28fathima@gmail.com>
2023-09-30 23:27:22 +0530

    S

--- Thonny/practice2/New folder/one.py
+++ Thonny/practice2/New folder/one.py
@@ -42,21 +42,90 @@ while ch =='y' or ch == 'Y':
     ch = input("Enter more record? Y/N: ")
 f.close()
 '''
-import csv
-f = open('data.csv', 'r')
+'''import csv 
+f = open('product.csv','r')
 d = csv.reader(f)
-next(d)  # Skip the header row
+next(d)
+found = False
+for row in d:
+    if int(row[2]) >= 300:
+        print("Product_id = ",row[0])
+        print("Product_name = ",row[1])
+        print("Price = ",row[2])
+        found = True
+        
 
-print("Students who scored marks more than 80 are: ")
+if not found :
+    print("Record not found")
+'''
+'''import csv
+f = open('data1.csv','r')
+d = csv.reader(f)
+next(d)
+s = 0
+for row in d:
+   s+=1
+
+print("Number of records are: ",s)
+f.close()'''
+
+'''import csv
+import os
+f = open('data1.csv','r')
+f1 = open('temp1.csv','w')
+d = csv.reader(f)
+d1 = csv.writer(f1)
+next(f)
+s = 0
+roll_no = int(input("Enter the roll number: "))
+mn = input("Enter the modified name: ")
+mm = int(input("Enter modified marks: "))
+mr = [roll_no,mn,mm]
+header = ["Roll number","Name","Marks"]
+d1.writerow(header)
 for i in d:
-    if int(i[4]) >= 80:  # Check if marks are more than 80
-        print("Roll number: ",i[0])
-        print("Name: ",i[1])
-        print("Class: ",i[2])
-        print("Section: ",i[3])
-        print("Marks: ",i[4])
-
+    if int(i[0]) == roll_no:
+        d1.writerow(mr)
+    else:
+        d1.writerow(i)
 f.close()
+f1.close()
 
+os.remove('data1.csv')
+os.rename('temp.csv','data1.csv')
+'''
+# import csv
+# f = open('temp1.csv','r')
+# d = csv.reader(f)
+# next(d)
+# max=0
+# for row in d:
+#     if int(row[2])>max:
+#         max=int(row[2])
+        
+# f.close()
+# f = open('temp1.csv','r')
+# d = csv.reader(f)
+# next(d)
+# for row in d:
+#     if int(row[2])==max:
+#         print(row)
+# f.close()
 
+'''import csv
+f = open('data.csv','r')
+d = csv.reader(f)
+for row in d:
+    print(','.join(row))
+    print(type(','.join(row)))
+    '''
+
+'''import csv
+field = ['Name','Class','Section','Marks']
+rows = [['Fathim','12','A','96'],['Lubna','13','C','90'],['Naseera','14','B','50']]
+f = open('data10.csv','w',newline='')
+wr = csv.writer(f)
+wr.writerows(field)
+wr.writerows(rows)
+f.close()'''
 
