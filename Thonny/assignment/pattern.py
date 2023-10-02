81223c3c2b7a24010103370ea588bd9a7663ae9b
Fathima28kousar <kousar28fathima@gmail.com>
2023-10-02 18:50:09 +0530

    S

---
+++ Thonny/assignment/pattern.py
@@ -0,0 +1,75 @@
+#1
+'''for i in range(10):
+    print("*",end="")
+'''
+#2
+'''for i in range(3):
+    for j in range(3):
+        print("A",end=" ")
+    print()   
+'''
+#3
+'''for i in range(4):
+    for j in range(4):
+        print("*",end=" ")
+    print()'''
+
+#4
+'''for i in range(5):
+    for i in range(5):
+        print("5",end=" ")
+    print()'''
+
+#5
+'''for i in range(1,5):
+    for j in range(1,5):
+        print(i,end=" ")
+    print()'''
+
+#6
+'''for i in range(5,0,-1):
+    for j in range(5,0,-1):
+        print(i,end=" ")
+    print()'''
+#7
+'''for i in range(5):
+    for j in range(i+1):
+        print("*",end="")
+    print()'''
+#8
+'''for i in range(4):
+    for j in range(i+1):
+        print("A",end="")
+    print()'''
+
+#9
+'''for i in range(5,1,-1):
+    for j in range(i-1):
+        print("*",end=" ")
+    print()
+'''
+#10
+'''letters = ["A","B","C","D"]
+for i in range(len(letters)):
+    for j in range(i+1):
+        print(letters[i],end=" ")
+    print()'''
+
+#11
+'''for i in range(1,5):
+    for j in range(i):
+        print(i,end="")
+    print()'''
+#12
+'''for i in range(1,6):
+    for j in range(i):
+        print(j+1,end=" ")
+    print()'''
+#13
+'''list = ["","A","B","C","D"]
+for i in range(1, len(list)):
+    for j in range(i):
+        print(list[j+1], end=" ")
+    print()
+'''
+
--- only practice/practice-1db/one.py
+++ only practice/practice-1db/one.py
@@ -1,24 +1,25 @@
-import mysql.connector
+# import mysql.connector
 
-connection = mysql.connector.connect(host="localhost", user="root", password="FKW#84#kousar", database="health")
-cursor = connection.cursor()
+# connection = mysql.connector.connect(host="localhost", user="root", password="FKW#84#kousar", database="health")
+# cursor = connection.cursor()
 
-# Check if the table 'vice' already exists
-cursor.execute("SHOW TABLES LIKE 'vice'")
-table_exists = cursor.fetchone()
+# # Check if the table 'vice' already exists
+# cursor.execute("SHOW TABLES LIKE 'vice'")
+# table_exists = cursor.fetchone()
 
-if not table_exists:
-    create_table = """
-        CREATE TABLE vice(
-            Hospital_Id INT UNSIGNED NOT NULL,
-            name VARCHAR(32),
-            bed_count INT,
-            PRIMARY KEY(Hospital_Id)
-        )
-    """
-    cursor.execute(create_table)
+# if not table_exists:
+#     create_table = """
+#         CREATE TABLE vice(
+#             Hospital_Id INT UNSIGNED NOT NULL,
+#             name VARCHAR(32),
+#             bed_count INT,
+#             PRIMARY KEY(Hospital_Id)
+#         )
+#     """
+#     cursor.execute(create_table)
 
-# Rest of your code for inserting data
+# # Rest of your code for inserting data
 
-cursor.close()
-connection.close()
+# cursor.close()
+# connection.close()
+
