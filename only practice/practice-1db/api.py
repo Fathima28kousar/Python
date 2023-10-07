b202ebe2415d780e671e2995eff34dbcc2ae71d3
Fathima28kousar <kousar28fathima@gmail.com>
2023-10-07 14:58:36 +0530

    S

--- only practice/practice-1db/api.py
+++ only practice/practice-1db/api.py
@@ -127,89 +127,24 @@ Requied filed: None
 #             print("You loose!")
 #             print("The correct word is: ",word)'''
 
-import random
-from collections import Counter
+# '''import re
+# data = '''abb Bengaluru airoepb_zebra fizzy  685904 049airoepb'''
+# pattern = r'\d'
+# match = re.findall(pattern,data)
+# print(match)
 
-someWords = '''
-apple banana mango strawberry
-orange grape pineapple apricot lemon coconut watermelon
-cherry papaya berry peach lychee muskmelon
+'''import re
+ip = '123.04483.0584.033.033.05.06.0'
+string = re.sub('\.[0]*','.',ip)
+print(string)'''
+'''import re 
+data = '56489 5789'
+pattern = r'^5*'
+match = re.findall(pattern,data)
+print(match)
 '''
-
-someWords = someWords.split()
-word = random.choice(someWords)
-
-if __name__ == '__main__':
-    print('Guess the word! HINT: The word is a name of a fruit')
-
-    for i in word:
-        # For printing the empty spaces for letters of the word
-        print('_', end=' ')
-    print()
-
-    playing = True
-    # List for storing the letters guessed by the player
-    letterGuessed = ''
-    chances = len(word) + 2
-    correct = 0
-    flag = 0
-
-    try:
-        while chances != 0 and flag == 0:  # Flag is updated when the word is correctly guessed
-            print()
-            chances -= 1
-
-            try:
-                guess = str(input('Enter a letter to guess: '))
-            except:
-                print('Enter only a letter!')
-                continue
-
-            # Validation of the guess
-            if not guess.isalpha():
-                print('Enter only a LETTER')
-                continue
-            elif len(guess) > 1:
-                print('Enter only a SINGLE letter')
-                continue
-            elif guess in letterGuessed:
-                print('You have already guessed that letter')
-                continue
-
-            # If letter is guessed correctly
-            if guess in word:
-                # k stores the number of times the guessed letter occurs in the word
-                k = word.count(guess)
-                for _ in range(k):
-                    letterGuessed += guess  # The guessed letter is added as many times as it occurs
-
-            # Print the word
-            for char in word:
-                if char in letterGuessed and (Counter(letterGuessed) != Counter(word)):
-                    print(char, end=' ')
-                    correct += 1
-                # If user has guessed all the letters
-                # Once the correct word is guessed fully,
-                elif Counter(letterGuessed) == Counter(word):
-                    # The game ends, even if chances remain
-                    print("The word is:", end=' ')
-                    print(word)
-                    flag = 1
-                    print('Congratulations, You won!')
-                    break  # To break out of the for loop
-                    break  # To break out of the while loop
-                else:
-                    print('_', end=' ')
-
-        # If user has used all of their chances
-        if chances <= 0 and (Counter(letterGuessed) != Counter(word)):
-            print()
-            print('You lost! Try again..')
-            print('The word was {}'.format(word))
-
-    except KeyboardInterrupt:
-        print()
-        print('Bye! Try again.')
-
-
-        
+import re 
+pattern = r'dog|cat'
+data = 'dog Dog cat Cat'
+match = re.findall(pattern,data,re.IGNORECASE)
+print(match)
--- w3resource/2condition_loops.py
+++ w3resource/2condition_loops.py
@@ -5,6 +5,22 @@ for x in range(1500,2701):
         print(x,end=" ")'''
 
 #2
+''''''''''def convert_temperature(value,unit):
+    if unit == "C" or "c":
+        return (value*9/5) +32
+    elif unit == "F" or "f":
+        return (value-32) *5/9
+    else:
+        return None
+unit = input("Enter the unit (C or F): ")
+value = float(input("Enter the temperature value: "))
+converted_value = convert_temperature(value,unit)
+
+if converted_value is not None:
+    print(f"{value} {unit} is equal to {converted_value:.2f} {'F' if unit == 'C' else 'C'}")
+else:
+    print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")'''''''''
+
 #3
 '''import random
 secret_number = random.randint(1,10)
@@ -60,6 +76,19 @@ for i in datalist:
         a.append(i)
     for i in a:
         print(i**2,end="\n")    '''
+#9
+'''def fibo(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+
+n = int(input("Enter the number of terms: "))
+for i in range(1, n + 1):
+    print(fibo(i), end=" ")'''
+
 
 #10
 '''for i in range(51):
@@ -73,7 +102,163 @@ for i in datalist:
         print("Buzz")
         continue
     print(i)'''
-    
+#12
+'''L =[]
+while True:
+    line = input("Enter a sequence (or 'q' to quit):")
+    if line.lower() =='q':
+        break
+    L.append(line.lower())
+print("Sentences in lowercase: ")
+for sentence in L:
+    print(sentence)'''
+'''binary_numbers = input("Enter a sequence of comma-separated 4-digit binary numbers: ")
+binary_list = binary_numbers.split(',')
+divisible_by_5 = []
+for binary in binary_list:
+    decimal_number = int(binary, 2)
+    if decimal_number % 5 == 0:
+        divisible_by_5.append(binary)
+result = ','.join(divisible_by_5)
+print("Numbers divisible by 5:", result)'''
+#13
+'''num = int(input("Enter the 4 digit binary numbers separated by comas: "))
+num_list = num.split(',')
+divisible_by_5 =[]
+for num in num_list:
+    decimal = int(num,2)
+
+    if decimal%5==0:
+        divisible_by_5.append(num)
+result = ','.join(divisible_by_5)
+print("Numbers divisible by 5: ",result)'''
+#14
+'''inp = input("Enter your desired sentences: ")
+letters =[]
+digits = []
+count1 = 0
+count =0
+for i in inp:
+    if i.isalpha():
+        count+=1
+        letters.append(i)
+    if i.isnumeric():
+        count1+=1
+        digits.append(i)
+print(letters)
+print("the number of letters are: ",count)
+print(digits)
+print("the number of digits are: ",count1)'''
+#15
+'''import re
+p= input("Input your password: ")
+x = True
+while x:  
+    if (len(p)<6 or len(p)>12):
+        break
+    elif not re.search("[a-z]",p):
+        break
+    elif not re.search("[0-9]",p):
+        break
+    elif not re.search("[A-Z]",p):
+        break
+    elif not re.search("[$#@]",p):
+        break
+    elif re.search("\s",p):
+        break
+    else:
+        print("Valid Password")
+        x=False
+        break
+if x:
+    print("Not a Valid Password")'''
+''''''
+#16
+'''L =[]
+for i in range(100,401):
+    s = str(i)
+    if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0):
+        L.append(s)
+print(','.join((L)))'''
+#31
+'''human = float(input("Enter the human years: "))
+if human<0:
+    print("Age must be postitve: ")
+    exit()
+elif human<=2:
+    dog = human*10.5
+else:
+    dog = 21 +(human-2)*4
+print("dogs age in dog years is:",dog)'''
+#32
+'''inputt = input("enter: ")
+if inputt == 'a' or inputt == 'e' or inputt == 'i' or inputt == 'u'or inputt == 'o':
+    print(inputt,"is a vowel")
+else:
+    print(inputt,"is a consonant")'''
+#33
+'''month = input("Enter the month: ")
+if  month== "February":
+    print("28/29 days")
+elif month in ("April", "June", "September", "November"):
+    print("30 days")
+elif month in ("January", "March", "May", "July", "August", "October", "December"):
+    print("31 days")
+else:
+    print("Wrong month")'''
+#34
+'''def sum(x,y):
+    sum = x+y
+    if sum in range(15,20):
+        return 20
+    else:
+        return sum
+
+print(sum(10,6))
+print(sum(17,5))
+'''
+#35
+'''str = input("Enter the number: ")
+if str.isnumeric():
+    print("It is an interger")
+else:
+    print("It is not an integer")
+'''
+#36    
+'''print("Input lengths of the triangle sides: ")
+x = int(input("x: "))
+y = int(input("y: "))
+z = int(input("z: "))
+if x==y==z:
+    print("Equalene triangle")
+elif x==y or y==z or z==x:
+    print("Isosceles triangle")
+else :
+    print("Scalene triangle")
+'''
+#37
+print("The months are: J")
+month = input("Enter the month: ")
+day = int(input("Enter the day: "))
+if month in ('January', 'February', 'March'):
+    season='winter'
+elif month in ('April', 'May', 'June'):
+    season = 'spring'
+elif month in ('July', 'August', 'September'):
+    season = 'summer'
+else:
+    season ='autumn'
+if (month == 'March') and (day > 19):
+	season = 'spring'
+elif (month == 'June') and (day > 20):
+	season = 'summer'
+elif (month == 'September') and (day > 21):
+	season = 'autumn'
+elif (month == 'December') and (day > 20):
+	season = 'winter'
+
+print("Season is",season)
+
 # m = int(input("Enter the number of rows: "))
 # n = int(input("Enter the number of columns: "))
 # i = range(0,m-1)
@@ -103,30 +288,9 @@ while True:
     input_ =input("Enter 0 to finish: ")
     if input_== "0":
         break'''
-#36    
-'''print("Input lengths of the triangle sides: ")
-x = int(input("x: "))
-y = int(input("y: "))
-z = int(input("z: "))
-if x==y==z:
-    print("Equalene triangle")
-elif x==y or y==z or z==x:
-    print("Isosceles triangle")
-else :
-    print("Scalene triangle")
-'''
 
-#34
-'''def sum(x,y):
-    sum = x+y
-    if sum in range(15,20):
-        return 20
-    else:
-        return sum
 
-print(sum(10,6))
-print(sum(17,5))
-'''
+
 
 # for row in range(7):
 #     for col in range(5):
@@ -143,5 +307,6 @@ print(sum(17,5))
 #     for col in range(5):
 #         if (col==0 ) or (col==4 and (row!=0 and row!=6)):
 #             if 
-def print_full_name(first, last):
-    return(f"Hello {first} {last}! You just delved into python")
+'''def print_full_name(first, last):
+    return(f"Hello {first} {last}! You just delved into python")'''
+
